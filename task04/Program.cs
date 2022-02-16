f7e45139f4f24d797970e07f7dc8de15342c9fcb
IvanKurylev93 <kuiv10@mail.ru>
2022-02-16 22:04:40 +0300

    Задача 4

---
+++ task04/Program.cs
@@ -0,0 +1,20 @@
+﻿//В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+Console.Clear();
+int[] mass = new int[10]; 
+ 
+for (int i = 0; i < mass.Length; i++) 
+{ 
+    mass[i] = new Random().Next(1, 99); 
+    Console.Write(mass[i] + " "); 
+} 
+Console.WriteLine(); 
+
+int max = mass[0];
+int min = mass[0];
+for (int i = 1; i < 10; i++)
+{
+    if (mass[i] < min) min = mass[i];
+    if (mass[i] > max) max = mass[i];
+}
+            
+Console.WriteLine("Разница между максимальным и минимальным = {0}", max - min);
