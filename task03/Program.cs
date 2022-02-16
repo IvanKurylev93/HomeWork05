2b10f7cfe7053c48476664524d4febda3685126a
IvanKurylev93 <kuiv10@mail.ru>
2022-02-16 22:04:14 +0300

    Задача 3

---
+++ task03/Program.cs
@@ -0,0 +1,18 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции;
+int[] mass = new int[10]; 
+ 
+for (int i = 0; i < mass.Length; i++) 
+{ 
+    mass[i] = new Random().Next(99, 899); 
+    Console.Write(mass[i] + " "); 
+} 
+Console.WriteLine(); 
+int summ = 0;
+for (int i = 0; i < mass.Length; i++)
+{
+    if (i%2==1)
+    {
+        summ=summ+mass[i];
+    }
+}
+Console.WriteLine(summ);
